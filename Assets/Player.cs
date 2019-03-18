5a14286d0c7103ce07977fb498786316f9f6fa73
dtan_yh85 <darren243@hotmail.com>
2019-03-18 21:35:27 +0800

    Video 191 - Animation State In Code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,13 +5,21 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // Config
     [SerializeField] float runSpeed = 5f;
-    Rigidbody2D myRigidBody;
 
-    // Start is called before the first frame update
+    // State
+    bool isAlive = true;
+
+    // Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+    // Message than methods
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,6 +34,9 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
     }
 
     private void FlipSprite()
