5c948b8dc09d5c0c22ae29efaeaf9e9ddcf3047b
dtan_yh85 <darren243@hotmail.com>
2019-03-19 22:22:19 +0800

    Video 192 - Jumpy Jumpy

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     // Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
     // State
     bool isAlive = true;
@@ -27,6 +28,7 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Jump();
     }
 
     private void Run()
@@ -37,6 +39,15 @@ public class Player : MonoBehaviour
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f,jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
