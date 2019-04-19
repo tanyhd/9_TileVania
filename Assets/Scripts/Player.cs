8f2678a20fee4efacc1ff4b76e256b7fbb13370e
dtan_yh85 <darren243@hotmail.com>
2019-04-19 16:09:59 +0800

    Video 204 - Adding Hazards

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -93,7 +93,7 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Death");
